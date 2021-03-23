4662185ac93a339a1b835ef1a07ff50fc5e2c09e
ranjith180295 <ranjith180295@gmail.com>
2021-03-23 15:56:30 +0530

    2 files modified

--- Home/Home.js
+++ Home/Home.js
@@ -1,40 +1,37 @@
-import React from 'react';
-import { render } from 'react-dom';
-import LstData from '../AddEdit/List';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';  
+import React from "react";
+import { render } from "react-dom";
+import LstData from "../AddEdit/List";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-class Home extends React.Component
-{
-  constructor(props)
-  {
+class Home extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={
-      userName:''
+    this.state = {
+      userName: ""
     };
   }
 
   componentDidMount() {
-      this.setState({ 
-        userName : localStorage.getItem('user')
-      });
-  };
-  
-  render()
-  {
-    return ( 
-      <div>
-      <h1> Hi User {this.state.userName}, </h1>
+    this.setState({
+      userName: localStorage.getItem("user")
+    });
+  }
 
-      <p>
-          <Link to="/login">Logout</Link>
-      </p>
-      
-      <div >
-        <LstData />
+  render() {
+    return (
+      <div>
+        <h1> Hi User {this.state.userName}, </h1>
+
+        <p>
+          <Link to="/">Logout</Link>
+        </p>
+
+        <div>
+          <LstData />
+        </div>
       </div>
-      </div>
-     );
-  };
-};
+    );
+  }
+}
 
 export default Home;
--- LogIn/LogIn.js
+++ LogIn/LogIn.js
@@ -1,58 +1,49 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';  
-import './style';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./style";
 
-class LoginCom extends React.Component
-{
-  constructor(props)
-  {
+class LoginCom extends React.Component {
+  constructor(props) {
     super(props);
 
     this.state = {
-      username : '',
-      password :'',
+      username: "",
+      password: "",
       logInClick: false,
-      error:null
+      error: null
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.ValidateLogin = this.ValidateLogin.bind(this);
-  };
-
-
-ValidateLogin()
-{
-  this.setState({logInClick:true,error:''});
-  const { username, password, returnUrl } = this.state;
-
-  if(!username || !password)
-  {
-    return;
   }
 
-  if(username =="Ranjith" && password =="123")
-  {
-    localStorage.setItem('user', username);
+  ValidateLogin() {
+    this.setState({ logInClick: true, error: "" });
+    const { username, password, returnUrl } = this.state;
 
-    localStorage.setItem('isLoggedin', 1);
-debugger;
-    this.props.history.push('/Home');
+    if (!username || !password) {
+      return;
+    }
+
+    if (username == "Ranjith" && password == "123") {
+      localStorage.setItem("user", username);
+
+      localStorage.setItem("isLoggedin", 1);
+      debugger;
+      this.props.history.push("/Home");
+    } else {
+      this.setState({ error: "Invalid Credentials" });
+    }
+    //this.setState({isLoggedIn : true});
+    // this.state.isLoggedIn = true;
+    // alert(this.state.isLoggedIn);
   }
-  else
-  {
-    this.setState({error:'Invalid Credentials'});
+
+  Reset() {
+    location.reload();
   }
-  //this.setState({isLoggedIn : true});
-  // this.state.isLoggedIn = true;
-  // alert(this.state.isLoggedIn);
-};
 
-Reset()
-{
-  location.reload();
-};
-
-handleChange(event) {
+  handleChange(event) {
     this.setState({
       [event.target.name]: [event.target.value]
     });
@@ -60,32 +51,55 @@ handleChange(event) {
     //console.log(this);
   }
 
-  render()
-  {
+  render() {
     const { username, password, logInClick, error } = this.state;
- let a = localStorage.getItem('isLoggedin') != 0 ? 'display:none' : 'display:block';
-    return (<div className="divMain" style={{a}}> 
-    <h2>Login</h2>
-    <div className="divtxtBox" >
-      <input className="txtBox" name="username"  type="text" id="txtLogInID" placeholder="Enter Name" value={this.state.username} onChange = {this.handleChange}/>      
-      {logInClick && !username && <div className="ErrorClass">
-      User Name is Required </div>}
+    let a =
+      localStorage.getItem("isLoggedin") != 0
+        ? "display:none"
+        : "display:block";
+    return (
+      <div className="divMain" style={{ a }}>
+        <h2>Login</h2>
+        <div className="divtxtBox">
+          <input
+            className="txtBox"
+            name="username"
+            type="text"
+            id="txtLogInID"
+            placeholder="Enter Name"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          {logInClick && !username && (
+            <div className="ErrorClass">User Name is Required </div>
+          )}
+        </div>
+        <div className="divtxtBox">
+          <input
+            className="txtBox"
+            name="password"
+            type="password"
+            id="txtLogInPwd"
+            value={this.state.password}
+            onChange={this.handleChange}
+            placeholder="Enter Password"
+          />
+          {logInClick && !password && (
+            <div className="ErrorClass">User Password is Required </div>
+          )}
+        </div>
+        <div className="divtxtBox">
+          <button className="Loginbutton" onClick={this.ValidateLogin}>
+            LogIn
+          </button>
+          <button className="Loginbutton" onClick={this.Reset}>
+            Reset
+          </button>
+        </div>
+        {error && <div className="ErrorClass">{error} </div>}
       </div>
-      <div className="divtxtBox">
-      <input className="txtBox" name="password"  type="password" id="txtLogInPwd" 
-      value={this.state.password} onChange = {this.handleChange}placeholder="Enter Password"/>      
-      {logInClick && !username && <div className="ErrorClass">
-      User Password is Required </div>}
-  </div>
-  <div className="divtxtBox">
-      <button className="Loginbutton" onClick={this.ValidateLogin}>LogIn</button>
-      <button className="Loginbutton" onClick={this.Reset}>Reset</button>
-      </div>
-      {error && <div className="ErrorClass">
-      {error} </div>}
-     </div>     
-     );
-  };
+    );
+  }
 }
 
 // class LogInFinalComp extends React.Component{
